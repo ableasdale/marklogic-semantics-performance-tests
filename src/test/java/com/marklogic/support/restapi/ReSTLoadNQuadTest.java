f1f85dba96b43c095dcb3b814a8b3c3d9461715e
Alex Bleasdale <ableasdale@marklogic.com>
2017-07-25 23:57:15 +0100

    reintroducing many of the disabled tests

--- src/test/java/com/marklogic/support/restapi/ReSTLoadNQuadTest.java
+++ src/test/java/com/marklogic/support/restapi/ReSTLoadNQuadTest.java
@@ -18,14 +18,14 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 @Tag("nquads")
 @MarkLogicReST
-@DisplayName("Benchmarking performance when loading NQuad (.nq) files using the ReST API endpoint")
+@DisplayName("Benchmarking performance when loading N-Quads (.nq) files using the ReST API endpoint")
 class ReSTLoadNQuadTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load to load a 20.5MB NQuad file (1_86286.nq)")
+    @DisplayName("Using the ReST API to load to load a 20.5MB N-Quads file (1_86286.nq)")
     void testLoadingSampleOne() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(20), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/1_86286.nq"));
         assertEquals(204, res.getStatus());
@@ -35,7 +35,7 @@ class ReSTLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load a 3.3MB NQuad file (2_12770.nq)")
+    @DisplayName("Using the ReST API to load a 3.3MB N-Quads file (2_12770.nq)")
     void testLoadingSampleTwo() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(10), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/2_12770.nq"));
         assertEquals(204, res.getStatus());
@@ -45,7 +45,7 @@ class ReSTLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load a 13.9MB NQuad file (3_54187.nq)")
+    @DisplayName("Using the ReST API to load a 13.9MB N-Quads file (3_54187.nq)")
     void testLoadingSampleThree() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(15), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/3_54187.nq"));
         assertEquals(204, res.getStatus());
@@ -55,7 +55,7 @@ class ReSTLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load a 35.3MB NQuad file (4_138495.nq)")
+    @DisplayName("Using the ReST API to load a 35.3MB N-Quads file (4_138495.nq)")
     void testLoadingSampleFour() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(35), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/4_138495.nq"));
         assertEquals(204, res.getStatus());
@@ -65,7 +65,7 @@ class ReSTLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load a 16MB NQuad file (5_63578.nq)")
+    @DisplayName("Using the ReST API to load a 16MB N-Quads file (5_63578.nq)")
     void testLoadingSampleFive() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(20), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/5_63578.nq"));
         assertEquals(204, res.getStatus());
@@ -75,7 +75,7 @@ class ReSTLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the ReST API to load a 31.6MB NQuad file (6_125268.nq)")
+    @DisplayName("Using the ReST API to load a 31.6MB N-Quads file (6_125268.nq)")
     void testLoadingSampleSix() {
         ClientResponse res = assertTimeoutPreemptively(ofSeconds(35), () -> MarkLogicReSTApiClientProvider.createPostForNQuads("nquads/6_125268.nq"));
         assertEquals(204, res.getStatus());
--- src/test/java/com/marklogic/support/sesame/SesameLoadNQuadTest.java
+++ src/test/java/com/marklogic/support/sesame/SesameLoadNQuadTest.java
@@ -10,15 +10,11 @@ import com.marklogic.support.providers.MarkLogicSesameRepositoryProvider;
 import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParseException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.lang.invoke.MethodHandles;
 
 import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,15 +27,14 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 @Tag("nquads3")
 @MarkLogicSesame
 @DisplayName("Benchmarking performance when loading N-Quad (.nq) files using the Sesame Repository API")
-public class SesameLoadNQuadTest {
+class SesameLoadNQuadTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    //private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load a 41.8MB N-Quads file (sider-indications_raw.nq)")
-    public void testLoadingMediumNQuadsFile() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 41.8MB N-Quads file (sider-indications_raw.nq)")
+    void testLoadingMediumNQuadsFile() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(20), () -> {
             conn.add(Utils.getFileReader("nquads/sider-indications_raw.nq"), "", RDFFormat.NQUADS);
@@ -48,11 +43,10 @@ public class SesameLoadNQuadTest {
         conn.close();
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load a 262.1MB N-Quads file (sider-label_mapping.nq)")
-    public void testLoadingLargeNQuadsFile() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 262.1MB N-Quads file (sider-label_mapping.nq)")
+    void testLoadingLargeNQuadsFile() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/sider-label_mapping.nq"), "", RDFFormat.NQUADS);
@@ -61,13 +55,10 @@ public class SesameLoadNQuadTest {
         conn.close();
     }
 
-    // Specific tests below:
-
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load toDelete.nq file (toDelete.nq)")
-    public void testLoadingtoDeleteFile() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 28.8MB N-Quads file (toDelete.nq)")
+    void testLoadingtoDeleteFile() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/toDelete.nq"), "", RDFFormat.NQUADS);
@@ -76,23 +67,23 @@ public class SesameLoadNQuadTest {
         conn.close();
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 1 (1_86286.nq)")
-    public void testLoadingSampleOne() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 20.5MB N-Quads file (1_86286.nq)")
+    void testLoadingSampleOne() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/1_86286.nq"), "", RDFFormat.NQUADS);
         });
         assertEquals(86286, SPARQLUtils.countAllTriples(conn));
         conn.close();
+        assertEquals(12003, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 2 (2_12770.nq)")
-    public void testLoadingSampleTwo() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 3.3MB N-Quads file (2_12770.nq)")
+    void testLoadingSampleTwo() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(60), () -> {
             conn.add(Utils.getFileReader("nquads/2_12770.nq"), "", RDFFormat.NQUADS);
@@ -104,8 +95,8 @@ public class SesameLoadNQuadTest {
 
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 3 (3_54187.nq)")
-    public void testLoadingSampleThree() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 13.9MB N-Quads file (3_54187.nq)")
+    void testLoadingSampleThree() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/3_54187.nq"), "", RDFFormat.NQUADS);
@@ -115,43 +106,42 @@ public class SesameLoadNQuadTest {
         assertEquals(12157, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 4 (4_138495.nq)")
-    public void testLoadingSampleFour() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 35.3MB N-Quads file (4_138495.nq)")
+    void testLoadingSampleFour() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/4_138495.nq"), "", RDFFormat.NQUADS);
         });
         assertEquals(138495, SPARQLUtils.countAllTriples(conn));
         conn.close();
+        assertEquals(25995, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 5 (5_63578.nq)")
-    public void testLoadingSampleFive() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 16MB N-Quads file (5_63578.nq)")
+    void testLoadingSampleFive() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/5_63578.nq"), "", RDFFormat.NQUADS);
         });
         assertEquals(63578, SPARQLUtils.countAllTriples(conn));
         conn.close();
+        assertEquals(12323, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
-    @DisplayName("Using the MarkLogic Sesame API to load Sample NQuad file 6 (6_125268.nq)")
-    public void testLoadingSampleSix() throws RepositoryException, IOException, RDFParseException {
+    @DisplayName("Using the MarkLogic RDF4J API to load a 31.6MB NQuad file (6_125268.nq)")
+    void testLoadingSampleSix() throws RepositoryException, IOException, RDFParseException {
         MarkLogicRepositoryConnection conn = MarkLogicSesameRepositoryProvider.getMarkLogicRepositoryConnection();
         assertTimeoutPreemptively(ofSeconds(80), () -> {
             conn.add(Utils.getFileReader("nquads/6_125268.nq"), "", RDFFormat.NQUADS);
         });
         assertEquals(125268, SPARQLUtils.countAllTriples(conn));
         conn.close();
+        assertEquals(24263, MarkLogicReSTApiClientProvider.getGraphCount());
     }
-
 }
