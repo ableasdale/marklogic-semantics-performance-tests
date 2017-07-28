55563120429fed19db586bc3506f9607f5ee493e
Alex Bleasdale <ableasdale@marklogic.com>
2017-07-28 12:52:49 +0100

    Re-enabled Jena tests

--- src/test/java/com/marklogic/support/jena/JenaLoadTurtleTest.java
+++ src/test/java/com/marklogic/support/jena/JenaLoadTurtleTest.java
@@ -4,9 +4,9 @@ import com.marklogic.support.SPARQLUtils;
 import com.marklogic.support.annotations.Benchmark;
 import com.marklogic.support.annotations.MarkLogicJena;
 import com.marklogic.support.extensions.MarkLogicJenaExtension;
+import com.marklogic.support.providers.MarkLogicReSTApiClientProvider;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
@@ -18,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 /**
  * Created by ableasdale on 29/05/2017.
  */
+
 @Tag("turtle")
 @MarkLogicJena
 @DisplayName("Benchmarking performance when loading Turtle (.ttl) files using the Jena Client API")
@@ -29,6 +30,7 @@ class JenaLoadTurtleTest {
     void testLoadingSmallXTurtleFile() {
         assertTimeoutPreemptively(ofSeconds(10), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG, "src/main/resources/turtle/charging-stations-export-20170530-095533.ttl", Lang.TURTLE));
         assertEquals(8900, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
     @Benchmark
@@ -37,6 +39,7 @@ class JenaLoadTurtleTest {
     void testLoadingSmallTurtleFile() {
         assertTimeoutPreemptively(ofSeconds(18), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG, "src/main/resources/turtle/units.ttl", Lang.TURTLE));
         assertEquals(23485, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
     @Benchmark
@@ -45,27 +48,24 @@ class JenaLoadTurtleTest {
     void testLoadingMediumSizeTurtleFile() {
         assertTimeoutPreemptively(ofSeconds(60), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG, "src/main/resources/turtle/unescothes.ttl", Lang.TURTLE));
         assertEquals(75202, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
     @DisplayName("Using the Jena Client API to load a 51MB Turtle file (history.ttl)")
     void testLoadingLargeTurtleFile() {
-
-        // TODO This fails.. why?
-        //RDFDataMgr.read(dsg, "src/main/resources/nt/ron.nt", Lang.NTRIPLES);
-
         assertTimeoutPreemptively(ofSeconds(330), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG, "src/main/resources/turtle/history.ttl", Lang.TURTLE));
         assertEquals(391551, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 
-    @Disabled
     @Benchmark
     @RepeatedTest(2)
     @DisplayName("Using the Jena Client API to load a 130MB Turtle file (fulldump.ttl)")
     void testLoadingAnotherLargeTurtleFile() {
         assertTimeoutPreemptively(ofSeconds(1000), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG, "src/main/resources/turtle/fulldump.ttl", Lang.TURTLE));
         assertEquals(204122, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
     }
 }
