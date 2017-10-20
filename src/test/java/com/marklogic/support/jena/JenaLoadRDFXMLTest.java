754467efd39344b77fe9dfefcb01ef5814a8eebd
Alex Bleasdale <ableasdale@marklogic.com>
2017-10-20 21:24:00 +0100

    Added some RDFXML tests for Jena

---
+++ src/test/java/com/marklogic/support/jena/JenaLoadRDFXMLTest.java
@@ -0,0 +1,54 @@
+package com.marklogic.support.jena;
+
+import com.marklogic.support.annotations.Benchmark;
+import com.marklogic.support.annotations.MarkLogicJena;
+import com.marklogic.support.extensions.MarkLogicJenaExtension;
+import com.marklogic.support.providers.MarkLogicReSTApiClientProvider;
+import com.marklogic.support.util.SPARQLUtils;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
+
+import static java.time.Duration.ofSeconds;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+@Tag("jena")
+@MarkLogicJena
+@DisplayName("Benchmarking performance when loading Resource Description Framework (.rdf) files using the Jena Client API")
+public class JenaLoadRDFXMLTest {
+
+    @Benchmark
+    @RepeatedTest(2)
+    @DisplayName("Using the Jena Client API to load a 175KB RDF/XML file (countries.rdf)")
+    void testLoadingSmallRDFXMLFile() {
+        assertTimeoutPreemptively(ofSeconds(10), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG.getDefaultGraph(), "src/main/resources/rdfxml/countries.rdf", Lang.RDFXML));
+        assertEquals(9330, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(9330, MarkLogicReSTApiClientProvider.getTripleCount());
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
+    }
+
+    @Benchmark
+    @RepeatedTest(2)
+    @DisplayName("Using the Jena Client API to load a 189KB RDF/XML file (currencies.rdf)")
+    void testLoadingAnotherSmallRDFXMLFile() {
+        assertTimeoutPreemptively(ofSeconds(10), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG.getDefaultGraph(), "src/main/resources/rdfxml/currencies.rdf", Lang.RDFXML));
+        assertEquals(3231, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(3231, MarkLogicReSTApiClientProvider.getTripleCount());
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
+    }
+
+    @Disabled
+    @Benchmark
+    @RepeatedTest(2)
+    @DisplayName("Using the Jena Client API to load a 21.8MB RDF/XML file (peel.rdf)")
+    void testLoadingMediumRDFXMLFile() {
+        assertTimeoutPreemptively(ofSeconds(120), () -> RDFDataMgr.read(MarkLogicJenaExtension.DSG.getDefaultGraph(), "src/main/resources/rdfxml/peel.rdf", Lang.RDFXML));
+        assertEquals(271369, SPARQLUtils.countAllTriples(MarkLogicJenaExtension.DSG));
+        assertEquals(271369, MarkLogicReSTApiClientProvider.getTripleCount());
+        assertEquals(2, MarkLogicReSTApiClientProvider.getGraphCount());
+    }
+}
--- src/test/java/com/marklogic/support/rdf4j/RDF4JLoadRDFXMLTest.java
+++ src/test/java/com/marklogic/support/rdf4j/RDF4JLoadRDFXMLTest.java
@@ -21,7 +21,7 @@ import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
-@Tag("rdfxml2")
+@Tag("rdfxml")
 @MarkLogicRDF4J
 @DisplayName("Benchmarking performance when loading Resource Description Framework (.rdf) files using the RDF4J API")
 class RDF4JLoadRDFXMLTest {
